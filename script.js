570934bc12bbdb45ba2f4a0f4819249fbcc93d2a
Lavinia-81 <131885710+Lavinia-81@users.noreply.github.com>
2024-08-10 17:08:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
-  navbar.classList.toggle('active');
-  searchForm.classList.remove('active');
-  cartItem.classList.remove('active');
+  navbar.classList.add('active');
 }
 
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () => {
-  searchForm.classList.toggle('active');
+document.querySelector('#close-navbar').onclick = () => {
   navbar.classList.remove('active');
-  cartItem.classList.remove('active');
 }
 
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () => {
-  cartItem.classList.toggle('active');
-  navbar.classList.remove('active');
-  searchForm.classList.remove('active');
-
-  
-}
-
-window.onscroll = () => {
-  navbar.classList.remove('active');
-  searchForm.classList.remove('active');
-  cartItem.classList.remove('active');
-}
